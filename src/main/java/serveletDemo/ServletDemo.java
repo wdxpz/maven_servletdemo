ae61999ddb98db172568a52bf85bb4b89f58fed8
unknown <>
2016-05-04 16:58:11 +0800

    init servelet demo

---
+++ src/main/java/serveletDemo/ServletDemo.java
@@ -0,0 +1,41 @@
+package serveletDemo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletDemo extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		req.setCharacterEncoding("UTF-8");  
+        resp.setContentType("text/html;charset=utf-8");
+        
+        String action = req.getParameter("action");  
+        if("login_input".equals(action)) {  
+            req.getRequestDispatcher("login.jsp").forward(req , resp);  
+        } else if("login".equals(action)) {  
+            String name = req.getParameter("name");  
+            String password = req.getParameter("password");  
+              
+            System.out.println("name->" + name + ",password->" + password);
+        }
+    }
+
+}
+	
